f69067865ac150cba0e231f2da19ec28113f13f8
hayeong120 <jbg01200249@gmail.com>
2023-07-29 21:29:46 +0900

    ArrayExam5

---
+++ ArrayExam5.java
@@ -0,0 +1,17 @@
+class ArrayExam5 
+{
+	public static void main(String[] args) 
+	{
+//		int a[] = new int[5];
+//		System.out.println(a.length);
+//		a.length = 3;
+
+//		int a[] = new int[5]{1,2,3,4,5};	// error 선언, 생성, 초기화를 할 때는 자릿값 지정X
+
+//		int a[] = new int[];	// error	선언, 생성을 할 때는 자릿값 지정
+
+		int a[] = {10,20,30,40,50};
+		System.out.println(a.length);
+		System.out.println(a[7]);	// error
+	}
+}
