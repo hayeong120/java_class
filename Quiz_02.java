1b582bd72bb7e02b4baf54db2546cc4539701d66
hayeong120 <jbg01200249@gmail.com>
2023-08-04 10:02:11 +0900

    Quix_02

---
+++ Quiz_02.java
@@ -0,0 +1,11 @@
+public class Quiz_02 {
+	public static void main(String args[]) {
+
+		String name = "아이유";
+		int age = 31;
+
+		System.out.println("내가 좋아하는 연예인은 " + name + "이고, " + age + "세 입니다.");
+
+
+	} 
+}
