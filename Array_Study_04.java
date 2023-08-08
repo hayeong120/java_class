1175908be919dcfe4914ce47619bfdc3d5484f54
hayeong120 <jbg01200249@gmail.com>
2023-08-08 22:58:47 +0900

    Array_Study_04

---
+++ Array_Study_04.java
@@ -0,0 +1,25 @@
+class Array_Study_04 
+{
+	public static void main(String[] args) 
+	{
+//		int table[][] = new int[3][4];
+//		table[0][0] = 10;
+//		table[1][1] = 20;
+//		table[2][3] = table[0][0] + table[1][1];
+//
+//		System.out.println(table[0][0]);
+//		System.out.println(table[1][1]);
+//		System.out.println(table[2][3]);
+
+		int table[][] = { {1}, {2, 3}, {4, 5, 6, 7, 8} };
+		for (int i=0; i<table.length; i++)
+		{
+			for (int j=0; j<table[i].length; j++)	// 서술형!!!
+			{
+				System.out.print(table[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+}
