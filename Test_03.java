c8429403440484577aa34a151e993674e7e28235
hayeong120 <jbg01200249@gmail.com>
2023-08-15 23:17:46 +0900

    Test_03

---
+++ Test_03.java
@@ -0,0 +1,11 @@
+public class Test_03{
+	public static void main(String args[]) {
+		//System.out.println("1반 화이팅!");
+		
+		String group = "방탄소년단";
+		String name = "뷔(김태형)";
+		//System.out.println(group);
+		//System.out.println(name);
+		System.out.println(group + " " + name);
+	}
+} 
