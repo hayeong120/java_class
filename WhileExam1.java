fc2fa190e431eb86b9497e05e50078796e65bc36
hayeong120 <jbg01200249@gmail.com>
2023-08-18 00:06:34 +0900

    WhileExam1

---
+++ WhileExam1.java
@@ -0,0 +1,19 @@
+class WhileExam1 
+{
+	public static void main(String[] args) 
+	{
+//		초기값;
+//		while(비교식) {
+//			실행문;
+//			증감식;
+//		}
+
+		int i=1;
+		while(i<=5) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("i값 = " + i);
+
+	}
+}
