e97b95e49081252efa3443ccdb691af80edb266f
hayeong120 <jbg01200249@gmail.com>
2023-08-31 15:34:17 +0900

    For_Study_01

---
+++ for_Study_01.java
@@ -0,0 +1,50 @@
+
+public class for_Study_01 {
+
+	public static void main(String[] args) {
+		
+//		int i, sum=0;
+//		for(i=1; i<=10; i++) { 
+//			System.out.println(i);
+//			sum += i;
+//		}
+//		System.out.println();
+//		System.out.println("i값 = " + i);
+//		System.out.println("합계 = " + sum);
+		
+//		for(int i=65; i<=90; i++) {
+//			System.out.print((char)i + " ");
+//		}
+//		System.out.println();
+//		for(int i='A'; i<='Z'; i++) {
+//			System.out.print(i + " ");
+//		}
+		
+		// 이중 for문
+//		for(int m=1; m<=3; m++) {
+//			System.out.println(" m = " + m);
+//			for (int n=1; n<=4; n++) {
+//				System.out.print(" n = " + n);
+//			}
+//			System.out.println();
+//		}
+		
+//		for (char i=65; i<=90; i++) {
+//			System.out.print(i + " : ");
+//			for (char j=i; j<='Z'; j++) {
+//				System.out.print(j);
+//			} // for j
+//			System.out.println();
+//		} // for i
+
+		
+		// continue, break 연습
+//			for (int i=1; i<=10; i++) {
+//				if(i==7)
+//					continue;
+//				System.out.println(i);
+//			}
+		
+
+	}
+}
