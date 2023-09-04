a8eb8e9d963c24b1d3862dfa7460a5eb0ad0a7d5
hayeong120 <jbg01200249@gmail.com>
2023-09-04 16:47:14 +0900

    For_Study_02

---
+++ for_Study_02.java
@@ -0,0 +1,17 @@
+
+public class for_Study_02 {
+
+	public static void main(String[] args) {
+		
+		
+//		long startTime = System.currentTimeMillis();
+//		System.out.println(startTime/1000/60/60/24/365);
+//		for(int i<=210000000; i++) {
+//			System.out.println(i);
+//		}
+//		long endTime = System.currentTimeMillis();
+				
+		System.out.println("시작 시간 = " + startTime);
+		System.out.println("종료 시간 = " + endTime);
+		System.out.println("수행 시간 = " + (endTime - startTime));
+}
