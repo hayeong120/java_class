c2d94e8efc9e549b433d77d616fcb98c76e64c99
hayeong120 <jbg01200249@gmail.com>
2023-10-14 00:17:32 +0900

    ClassExam

---
+++ ClassExam.java
@@ -0,0 +1,15 @@
+
+public class ClassExam {
+
+	public static void main(String[] args) {
+		Student kim = new Student();
+		kim.name = "김현우";
+		kim.grade = 2; 
+		System.out.println("학생의 이름은 " + kim.name + ",이고 " + kim.grade + "학년입니다.");
+		//System.out.println(kim.ban);		// 0 출력
+		//System.out.println(kim.telephone);	// null 출력
+		
+		
+	}
+
+}
